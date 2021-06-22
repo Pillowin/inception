7c48a2190c99949c3f5cdcab1abd96fc344ac532
Antoine GAUTIER <agautier@student.42.fr>
2021-06-22 13:12:58 +0200

    feat: add both user in wp

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,12 +2,19 @@
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /bin/wp
 chmod +x /bin/wp
+
 mkdir -p /var/www/html
-chmod -R 777 /var/www/html
+chmod -R 755 /var/www/html
+
 rm -f /var/www/html/wp-config.php
 rm -f /var/www/html/wp-config-sample.php
-wp core download --path=/var/www/html/ --locale=fr_FR
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost="$DB_HOST" --path=/var/www/html/
- wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --path=/var/www/html
+
+wp core download --path=/var/www/html/ --locale=fr_FR 2>> log >> log
+
+wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --path=/var/www/html/ 2>> log >> log
+
+wp core install --url="$DOMAIN_NAME" --title="$TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_MAIL" --path="/var/www/html" 2>> log >> log
+
+wp user create $WP_USER $WP_USER@$DOMAIN_NAME --role="author" --user_pass="$WP_PASSWORD" --path="/var/www/html" 2>> log >> log
 
 exec php-fpm7 -F
